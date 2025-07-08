50df6f46cbc4fc09e68ebb9aba3cb95b1841990e
mikey404 <michaelangeloricamata@gmail.com>
2025-07-08 20:53:49 +0800

    Add files via upload

--- JAVA/06 - Random Number/RandomNumber.java
+++ JAVA/06 - Random Number/RandomNumber.java
@@ -1,1 +1,16 @@
+package RandomNumber.java;
+import java.util.Random;
 
+public class RandomNumber{
+   public static void main(String[] args){
+      
+      Random random = new Random();
+      
+      int min = 1;
+      int max = 10;
+      int number = random.nextInt(min, max + 1);
+      
+      System.out.println(number);
+      
+   }
+}

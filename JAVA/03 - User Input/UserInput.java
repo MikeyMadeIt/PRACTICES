09ccec26e5aa2c100e48951625ba64b1df01d5a6
mikey404 <michaelangeloricamata@gmail.com>
2025-07-08 19:08:27 +0800

    Add files via upload

--- JAVA/03 - User Input/UserInput.java
+++ JAVA/03 - User Input/UserInput.java
@@ -1,1 +1,19 @@
+package UserInput;
 
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+       
+        Scanner getUserInput = new Scanner(System.in);
+        
+        System.out.print("Name: ");
+        String getUserName = getUserInput.nextLine();
+        
+        System.out.print("Age: ");
+        int getUserAge = getUserInput.nextInt();
+        
+        System.out.println("Youe name is " + getUserName + " and you are " + getUserAge + " years old");
+    }
+    
+}

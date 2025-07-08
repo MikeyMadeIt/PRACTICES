9087b03024ce77b3681c8fed1150cca67b0e3739
mikey404 <michaelangeloricamata@gmail.com>
2025-07-08 20:45:44 +0800

    Add files via upload

--- JAVA/05 - If Statement/IfStatement.java
+++ JAVA/05 - If Statement/IfStatement.java
@@ -1,1 +1,32 @@
+package IfStatement.java;
 
+public class IfStatement{
+   public static void main(String[] args){
+      
+      boolean isEnrolled = true;
+      int age = 18;
+      
+      if(isEnrolled){
+         System.out.println("You are enrolled!");
+      }else{
+         System.out.println("You are NOT enrolled!");
+      }
+      
+      if(age >= 100){
+         System.out.println("You are probably dead!");
+      }
+      else if(age >= 60){
+         System.out.println("You are a Senior!");
+      }
+      else if(age >= 18){
+         System.out.println("You are an Adult!");
+      }
+      else if(age < 18){
+         System.out.println("You are a Minor!");
+      }
+      else if(age == 0){
+         System.out.println("You has NOt been born yet!");
+      }
+      
+   }
+}

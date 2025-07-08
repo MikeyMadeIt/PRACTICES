8c35f0f572e39eabbfd20cb81b7ac424792f31cf
mikey404 <michaelangeloricamata@gmail.com>
2025-07-08 20:36:34 +0800

    Add files via upload

---
+++ JAVA/04 - Arithmetic/Arithmethic.java
@@ -0,0 +1,32 @@
+package Arithmetic.java;
+
+public class Arithmetic{
+   public static void main(String[] args){
+      
+      int a, b, result1, result2, result3, result4, result5;
+      a = 20;
+      b = 10;
+      
+      // ADDITION
+      result1 = a + b;
+      
+      // MULTIPLICATION
+      result2 = a * b;
+      
+      // SUBTRACTION
+      result3 = a - b;
+      
+      // DIVISION
+      result4 = a / b;
+      
+      // MODULO
+      result5 = a % b;
+      
+      System.out.println("Addition: " + result1);
+      System.out.println("Multiplication: " + result2);
+      System.out.println("Subtraction: " + result3);
+      System.out.println("Division: " + result4);
+      System.out.println("Modulo: " + result5);
+      
+   }
+}

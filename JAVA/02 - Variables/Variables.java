f4c6cb6d57cfa4510deaa81962720ea5286a2503
mikey404 <michaelangeloricamata@gmail.com>
2025-07-08 19:04:44 +0800

    Add files via upload

--- JAVA/02 - Variables/Variables.java
+++ JAVA/02 - Variables/Variables.java
@@ -1,1 +1,25 @@
+package Variables.java;
 
+public class Variables{
+   public static void main(String[] args){
+      
+      // INTEGER
+      byte a = 123;
+      short b = 12345;
+      int c = 12345678910;
+      long d = 123456789101112;
+      
+      // FLOATING POINT
+      float e = 2.71828;
+      double f = 3.14159265359;
+      
+      // SYMBOLS AND LINES
+      String g = "Lebron James";
+      char h = 'D';
+      
+      // BOOLEAN
+      boolean i = true;
+      boolean j = false;
+      
+   }
+}

dbd34610085b327827c553cb69f7e9a9b730a072
mikey404 <michaelangeloricamata@gmail.com>
2025-07-09 09:03:46 +0800

    Update Variables.java

--- JAVA/02 - Variables/Variables.java
+++ JAVA/02 - Variables/Variables.java
@@ -6,11 +6,11 @@ public class Variables{
       // INTEGER
       byte a = 123;
       short b = 12345;
-      int c = 12345678910;
-      long d = 123456789101112;
+      int c = 12345678;
+      long d = 123456789101112L;
       
       // FLOATING POINT
-      float e = 2.71828;
+      float e = 2.71828F;
       double f = 3.14159265359;
       
       // SYMBOLS AND LINES

7cdee713aac6d752935fd250490d03c463a576c0
mikey404 <michaelangeloricamata@gmail.com>
2025-07-10 16:00:07 +0800

    Add files via upload

--- JAVASCRIPT/02 - Variables/Variables.js
+++ JAVASCRIPT/02 - Variables/Variables.js
@@ -1,1 +1,7 @@
+let name = "Michael Angelo";
+let age = 17;
+let program = "Computer Science";
 
+document.getElementById('text1').textContent = `Your Name is ${name}`;
+document.getElementById('text2').textContent = `Your Age is ${age} years old`;
+document.getElementById('text3').textContent = `Your Program is ${program}`;

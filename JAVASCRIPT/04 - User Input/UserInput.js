28502280b6e2a6823950239479d1b598dc46e691
mikey404 <michaelangeloricamata@gmail.com>
2025-07-10 16:03:31 +0800

    Add files via upload

--- JAVASCRIPT/04 - User Input/UserInput.js
+++ JAVASCRIPT/04 - User Input/UserInput.js
@@ -1,1 +1,11 @@
+// THE EASY WAY
+let username;
+username = window.prompt("What is your name?")
+console.log(`Your name is: ${username}`);
 
+// THE PROFESSIONAL WAY
+let username;
+document.getElementById('btn').onclick = function(){
+   username = document.getElementById('text').value;
+   document.getElementById('greet').textContent = `Hello, ${username}`;
+};

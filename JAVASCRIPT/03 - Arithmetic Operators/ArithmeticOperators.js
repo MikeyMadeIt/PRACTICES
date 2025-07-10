bd7678657da0c224ab2ab4b8f1a4f4cb92b94c9e
mikey404 <michaelangeloricamata@gmail.com>
2025-07-10 16:02:05 +0800

    Add files via upload

--- JAVASCRIPT/03 - Arithmetic Operators/ArithmeticOperators.js
+++ JAVASCRIPT/03 - Arithmetic Operators/ArithmeticOperators.js
@@ -1,1 +1,31 @@
+let x = 3;
+let y = 5;
+
+// ARITHMETIC OPERATORS
+result1 = x + y;
+result2 = x - y;
+result3 = x * y;
+result4 = x / y;
+result5 = x ** y;
+result6 = x % y;
+
+// AUGMENTED ASSIGNMENT OPERATORS
+x += 2;
+x -= 2;
+x *= 2;
+x /= 2;
+x **= 2;
+x %= 2;
+
+// INCREMENT & DECREMENT OPERATORS
+y++;
+y--;
+
+/*
+OPERATOR PRECEDENCE
+   1. parenthesis ()
+   2. exponent
+   3. multiplication, division & modulo
+   4. addition & subtraction
+*/
 

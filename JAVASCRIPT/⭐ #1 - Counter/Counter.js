2a1592effa6cd610aefd3d1fc67a05d91b0c0f04
mikey404 <michaelangeloricamata@gmail.com>
2025-07-11 09:46:16 +0800

    Add files via upload

--- JAVASCRIPT/⭐ #1 - Counter/Counter.js
+++ JAVASCRIPT/⭐ #1 - Counter/Counter.js
@@ -1,1 +1,18 @@
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const countLabel = document.getElementById('counter');
+const resetBtn = document.getElementById('reset');
+let count = 0;
 
+increaseBtn.onclick = function(){
+   count++;
+   countLabel.textContent = count;
+};
+decreaseBtn.onclick = function(){
+   count--;
+   countLabel.textContent = count;
+};
+resetBtn.onclick = function(){
+   count = 0;
+   countLabel.textContent = count;
+};

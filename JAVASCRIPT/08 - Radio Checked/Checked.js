b3293b596a7c50f834e74618bc1f8b7b97986436
mikey404 <michaelangeloricamata@gmail.com>
2025-07-12 18:29:20 +0800

    Add files via upload

--- JAVASCRIPT/08 - Radio Checked/Checked.js
+++ JAVASCRIPT/08 - Radio Checked/Checked.js
@@ -1,1 +1,22 @@
-
+ const visa = document.getElementById('visa');
+ const paypal = document.getElementById('paypal');
+ const gcash = document.getElementById('gcash');
+ const submit = document.getElementById('submit');
+ const output = document.getElementById('output');
+ let message;
+ 
+ submit.onclick = function() {
+    if(visa.checked){
+       message = "You select Visa as your payment";
+       output.textContent = message;
+    }else if(paypal.checked){
+       message = "You select Paypal as your payment";
+       output.textContent = message;
+    }else if(gcash.checked){
+       message = "You select Gcash as your payment";
+       output.textContent = message;
+    }else{
+       message = "Please select a payment";
+       output.textContent = message;
+    }
+ }

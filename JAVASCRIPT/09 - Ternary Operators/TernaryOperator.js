6d9688f4086f219109a3107702e496f0a352696c
mikey404 <michaelangeloricamata@gmail.com>
2025-07-13 11:26:40 +0800

    Add files via upload

--- JAVASCRIPT/09 - Ternary Operators/TernaryOperator.js
+++ JAVASCRIPT/09 - Ternary Operators/TernaryOperator.js
@@ -1,1 +1,15 @@
+let age = 18;
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+console.log(message);
 
+let time = 12;
+let greet = time >= 12 ? "Good Afternoon" : "Good Morning";
+console.log(greet);
+
+let student = true;
+let status = student ? "You are a student" : "You are NOT a student";
+console.log(status);
+
+let playMinecraft = true;
+let play = playMinecraft ? "You are playing Minecraft" : "You are NOT playing Minecraft";
+console.log(play);

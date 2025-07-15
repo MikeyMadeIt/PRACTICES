721d2581b31ddefa72917fc9a2c6569ad2c91dbf
mikey404 <michaelangeloricamata@gmail.com>
2025-07-15 18:28:18 +0800

    Create Array.java

---
+++ JAVA/21 - Array/Array.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args){
+       
+      String[] fruits = {"apple", "orange", "banana", "coconut"};
+
+      //fruits[0] = "pineapple";
+      //int numOfFruits = fruits.length;
+      //Arrays.sort(fruits);
+      //Arrays.fill(fruits, "pineapple");
+
+      for(String fruit : fruits){
+         System.out.println(fruit);
+      }
+   }
+}
